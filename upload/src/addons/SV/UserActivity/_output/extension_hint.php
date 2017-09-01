294a915585af29d3f263d232b8b0cc1c4144bda4
Xon <tazipper1@hotmail.com>
2017-09-02 07:42:23 +0800

    Store _output rather than _data in source control

---
+++ upload/src/addons/SV/UserActivity/_output/extension_hint.php
@@ -0,0 +1,22 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
+
+namespace SV\UserActivity\SV\ContentRatings\Pub\Controller
+{
+	class XFCP_Ratings extends \SV\UserActivity\XF\Pub\Controller\Ratings {}
+}
+
+namespace SV\UserActivity\XF\Pub\Controller
+{
+	class XFCP_Conversation extends \XF\Pub\Controller\Conversation {}
+	class XFCP_Post extends \XF\Pub\Controller\Post {}
+	class XFCP_Report extends \XF\Pub\Controller\Report {}
+	class XFCP_Thread extends \XF\Pub\Controller\Thread {}
+}
+
+namespace SV\UserActivity\XF\Repository
+{
+	class XFCP_SessionActivity extends \XF\Repository\SessionActivity {}
+}
