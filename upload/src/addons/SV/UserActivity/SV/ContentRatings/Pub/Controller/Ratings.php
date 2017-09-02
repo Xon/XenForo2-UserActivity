c5e598520418cf12b6b2dd14c2044f338b0f1e24
Xon <tazipper1@hotmail.com>
2017-09-02 10:49:10 +0800

    Support content ratings properly

--- upload/src/addons/SV/UserActivity/SV/ContentRatings/Pub/Controller/Ratings.php
+++ upload/src/addons/SV/UserActivity/SV/ContentRatings/Pub/Controller/Ratings.php
@@ -10,12 +10,39 @@ class Ratings extends XFCP_Ratings
     protected $activityInjector = [
         'controller' => 'SV\\ContentRatings\\Pub\\Controller\\Ratings',
         'type' => null,
-        'id' => null,
+        'id' => 'container_id',
         'actions' => ['like', 'list'],
     ];
     use ActivityInjector
     {
         preDispatchController as preDispatchControllerTrait;
+    }
+
+    /** @var ParameterBag $_params */
+    protected $_params;
+
+    /**
+     * @return array
+     */
+    protected function assertViewableContent($contentType, $contentId)
+    {
+        $list = parent::assertViewableContent($contentType, $contentId);
+        /** @var \XF\Mvc\Entity\Entity $content */
+        $content = $list[0];
+
+        if (is_callable([$content, 'getContainer']) && ($container = $content->getContainer()))
+        {
+            /** @var \XF\Mvc\Entity\Entity $container */
+
+            $this->activityInjector['type'] = $container->getEntityContentType();
+            $this->_params['container_id'] = $container->getEntityId();
+
+            /** @var \SV\UserActivity\Repository\UserActivity $userActivityRepo */
+            $userActivityRepo = $this->app->repository('SV\UserActivity:UserActivity');
+            $userActivityRepo->registerHandler($this->activityInjector['controller'], $this->activityInjector['type'], $this->activityInjector['id']);
+        }
+
+        return $list;
     }
 
     /**
@@ -24,13 +51,7 @@ class Ratings extends XFCP_Ratings
      */
     protected function preDispatchController($action, ParameterBag $params)
     {
-        $input = $this->filter([
-            'content_type' => 'str',
-            'content_id' => 'uint',
-        ]);
-        $this->activityInjector['type'] = $input['content_type'];
-        $this->activityInjector['id'] = $input['content_id'];
-
-        $this->preDispatchControllerTrait($action, $params);
+        $this->_params = $params;
+        parent::preDispatchController($action, $params);
     }
 }
