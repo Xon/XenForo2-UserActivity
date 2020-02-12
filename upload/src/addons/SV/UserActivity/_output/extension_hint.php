58865e46a1715714a1981c7f2954a6d48175d097
Xon <tazipper1@hotmail.com>
2020-02-12 10:13:32 +0800

    Sync extension_hint

--- upload/src/addons/SV/UserActivity/_output/extension_hint.php
+++ upload/src/addons/SV/UserActivity/_output/extension_hint.php
@@ -3,14 +3,14 @@
 // ################## THIS IS A GENERATED FILE ##################
 // DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
 
+namespace SV\UserActivity\NF\Calendar\Pub\Controller
+{
+	class XFCP_Event extends \NF\Calendar\Pub\Controller\Event {}
+}
+
 namespace SV\UserActivity\NF\Tickets\Pub\Controller
 {
 	class XFCP_Ticket extends \NF\Tickets\Pub\Controller\Ticket {}
-}
-
-namespace SV\UserActivity\NF\Calendar\Pub\Controller
-{
-    class XFCP_Event extends \NF\Calendar\Pub\Controller\Event {}
 }
 
 namespace SV\UserActivity\SV\ContentRatings\Pub\Controller
