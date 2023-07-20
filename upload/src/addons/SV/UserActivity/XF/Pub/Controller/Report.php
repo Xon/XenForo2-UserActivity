59f862b60818c16c90f7d343d52cc1932a751dfc
Xon <635541+Xon@users.noreply.github.com>
2023-07-20 13:12:18 +0800

    Add viewer counts to report view and in lists of reports.

--- upload/src/addons/SV/UserActivity/XF/Pub/Controller/Report.php
+++ upload/src/addons/SV/UserActivity/XF/Pub/Controller/Report.php
@@ -2,7 +2,13 @@
 
 namespace SV\UserActivity\XF\Pub\Controller;
 
+use SV\ReportCentreEssentials\Entity\ReportQueue as ReportQueueEntity;
+use SV\UserActivity\Repository\UserActivity as UserActivityRepo;
 use SV\UserActivity\UserActivityInjector;
+use SV\UserActivity\UserCountActivityInjector;
+use XF\Mvc\Entity\AbstractCollection;
+use XF\Mvc\Reply\View as ViewReply;
+use function is_array;
 
 class Report extends XFCP_Report
 {
@@ -14,4 +20,136 @@ class Report extends XFCP_Report
         'activeKey'  => 'report',
     ];
     use UserActivityInjector;
+
+    protected $countActivityInjector = [
+//        [
+//            'activeKey' => 'report-list',
+//            'type'      => 'report-queue',
+//            'actions'   => ['index', 'closed'],
+//            'fetcher'   => 'reportQueueFetcher',
+//        ],
+        [
+            'activeKey' => 'report-list',
+            'type'      => 'report',
+            'actions'   => ['index', 'closed'],
+            'fetcher'   => 'reportsFetcher',
+        ],
+        [
+            'activeKey' => 'report',
+            'type'      => 'report',
+            'actions'   => ['view'],
+            'fetcher'   => 'reportsFetcher'
+        ],
+    ];
+    use UserCountActivityInjector;
+
+    /** @noinspection PhpUnusedParameterInspection */
+    protected function reportsFetcher(ViewReply $response, string $action, array $config): array
+    {
+        $reportIds = [];
+
+        $reports = $response->getParam('openReports');
+        if ($reports instanceof AbstractCollection || is_array($reports))
+        {
+            foreach($reports as $report)
+            {
+                if ($report instanceof \XF\Entity\Report)
+                {
+                    $reportIds[$report->report_id] = true;
+                }
+            }
+        }
+
+        $reports = $response->getParam('closedReports');
+        if ($reports instanceof AbstractCollection || is_array($reports))
+        {
+            foreach($reports as $report)
+            {
+                if ($report instanceof \XF\Entity\Report)
+                {
+                    $reportIds[$report->report_id] = true;
+                }
+            }
+        }
+
+        $report = $response->getParam('report');
+        if ($report instanceof \XF\Entity\Report)
+        {
+            $reportIds[$report->report_id] = true;
+        }
+
+        return array_keys($reportIds);
+    }
+
+    /** @noinspection PhpUnusedParameterInspection */
+    protected function reportQueueFetcher(ViewReply $response, string $action, array $config): array
+    {
+        $queueIds = [];
+        if (\XF::isAddOnActive('SV/ReportCentreEssentials'))
+        {
+            $queues = $response->getParam('reportQueues');
+            if ($queues instanceof AbstractCollection || is_array($queues))
+            {
+                foreach($queues as $queue)
+                {
+                    if ($queue instanceof ReportQueueEntity)
+                    {
+                        $queueIds[$queue->queue_id] = true;
+                    }
+                }
+            }
+
+            $reportQueue = $response->getParam('reportQueue');
+            if ($reportQueue instanceof ReportQueueEntity)
+            {
+                $queueIds[$reportQueue->queue_id] = true;
+            }
+            $report = $response->getParam('report');
+            if ($report instanceof ReportQueueEntity)
+            {
+                $queueIds[$report->queue_id] = true;
+            }
+        }
+
+        return $queueIds;
+    }
+
+//    public function actionView(ParameterBag $params)
+//    {
+//        $response = parent::actionView($params);
+//
+//        if ($response instanceof ViewReply)
+//        {
+//            $report = $response->getParam('report');
+//            if ($report instanceof \XF\Entity\Report)
+//            {
+//                /** @var \SV\ReportCentreEssentials\XF\Entity\Report $report */
+//                $this->getUserActivityRepo()->pushViewUsageToParent($response, $report->ReportQueue, true);
+//            }
+//        }
+//
+//        return $response;
+//    }
+//
+//    protected function updateSessionActivity($action, ParameterBag $params, AbstractReply &$reply)
+//    {
+//        if ($reply instanceof ViewReply &&
+//            \XF::isAddOnActive('SV/ReportCentreEssentials') &&
+//            $params->get('queue_id') === null)
+//        {
+//            $reportQueue = $reply->getParam('reportQueue');
+//            if ($reportQueue instanceof ReportQueueEntity)
+//            {
+//                $params['queue_id'] = $reportQueue->queue_id;
+//            }
+//        }
+//
+//        parent::updateSessionActivity($action, $params, $reply);
+//    }
+
+    protected function getUserActivityRepo(): UserActivityRepo
+    {
+        /** @noinspection PhpIncompatibleReturnTypeInspection */
+        return $this->repository('SV\UserActivity:UserActivity');
+    }
 }
