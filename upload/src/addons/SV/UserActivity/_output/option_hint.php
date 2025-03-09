8d97bdc6aed0032ff7c9c9b3b3a2ee25c2f173b4
Xon <635541+Xon@users.noreply.github.com>
2025-03-10 06:43:15 +0800

    XF2.3 export

---
+++ upload/src/addons/SV/UserActivity/_output/option_hint.php
@@ -0,0 +1,30 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE OPTIONS IN THE CONTROL PANEL.
+
+/**
+ * @noinspection PhpMultipleClassDeclarationsInspection
+ * @noinspection PhpIllegalPsrClassPathInspection
+ */
+
+namespace XF;
+
+/**
+ * @property int|null $RainDD_UA_ForumViewPos Viewing container position (Forum)
+ * @property int|null $RainDD_UA_ThreadViewPos Viewing container position
+ * @property int|null $RainDD_UA_ThreadViewType Viewing display type
+ * @property int|null $SV_UA_Cutoff Viewing users cut-off
+ * @property bool|null $SV_UA_TrackRobots User activity tracks robots
+ * @property float|null $UA_fillFactor User activity fill factor (redis)
+ * @property float|null $UA_pruneChance User activity prune chance
+ * @property array|null $svUADisplayCounts Display viewer counts
+ * @property array{thread: bool, conversation: bool, report: bool, nf_ticket: bool, nf_calendar: bool}|null $svUADisplayUsers Display user list
+ * @property array{thread: bool, conversation: bool, report: bool, nf_ticket: bool, nf_calendar: bool}|null $svUAPopulateUsers Track user activity
+ * @property int|null $svUAThreadNodeTrackLimit Thread contribute limit
+ * @property int|null $svUserActivityReportViewPos Reports viewing container position
+ * @property bool|null $svUserActivityTrackGuests User activity tracks guests
+ */
+class Options
+{
+}
